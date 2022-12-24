2f1ec98e03561e3f946f05966ebc0d39a5256509
Abhay Goyal <agoyal4_be19@thapar.edu>
2022-12-24 21:30:09 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    capital = models.CharField(max_length=50)
+    area = models.IntegerField()
+    population = models.IntegerField()
+
